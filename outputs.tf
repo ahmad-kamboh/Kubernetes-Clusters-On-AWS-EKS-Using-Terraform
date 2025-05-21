623bf058307ec46a531eb4b0eed532d5dcdda76e
ahmadtahirkamboh <ahmadtahir4235@gmail.com>
2025-05-21 03:30:04 -0700

    Create outputs.tf

---
+++ outputs.tf
@@ -0,0 +1,14 @@
+output "eks_cluster_endpoint" {
+  description = "The endpoint for the EKS cluster"
+  value       = module.eks.cluster_endpoint
+}
+
+output "eks_cluster_name" {
+  description = "The name of the EKS cluster"
+  value       = module.eks.cluster_name
+}
+
+output "eks_cluster_certificate_authority_data" {
+  description = "The certificate authority data for the EKS cluster"
+  value       = module.eks.cluster_certificate_authority_data
+}
